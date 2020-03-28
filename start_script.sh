147ffca7d37f6719172018cbb6ed54005897aadf
loki <tienlocbui1110@gmail.com>
2020-03-29 00:42:50 +0700

    Upload script

--- start_script.sh
+++ start_script.sh
@@ -6,12 +6,19 @@ if [ "$EUID" -ne 0 ];
   exit
 fi
 
+#Update apt list
+apt-get update
+
+################## Install Field ############################
+#Install common
+apt-get install software-properties-common
+
 # Install lib to run javascript 
 apt-get install libc6-i386
 
-# use md5 as md5sum
-cd $(dirname $(which md5sum))
-cp md5sum md5
+# Install qemu-kvm to use Android Emu
+apt-get install qemu-kvm
+adduser $USER kvm
 
 # Show all system tray icon && remove duplicate wifi
 add-apt-repository ppa:yunnxx/elementary
@@ -19,3 +26,8 @@ apt update
 apt install indicator-application wingpanel-indicator-ayatana
 sed -i -e 's/OnlyShowIn=Unity;GNOME;/OnlyShowIn=Unity;GNOME;Pantheon;/g' /etc/xdg/autostart/indicator-application.desktop
 rm -f /etc/xdg/autostart/nm-applet.desktop
+
+###################### Util Field ############################
+# use md5 as md5sum
+cd $(dirname $(which md5sum))
+cp md5sum md5
